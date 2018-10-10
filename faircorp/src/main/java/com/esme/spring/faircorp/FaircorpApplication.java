0f7f610fdbbf2dca12be4251943cf3baee96f9aa
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-10-10 16:32:32 +0200

    Start TP BDD

--- faircorp/src/main/java/com/esme/spring/faircorp/FaircorpApplication.java
+++ faircorp/src/main/java/com/esme/spring/faircorp/FaircorpApplication.java
@@ -3,10 +3,13 @@ package com.esme.spring.faircorp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class FaircorpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FaircorpApplication.class, args);
+
+	    run(FaircorpApplication.class, args);
 	}
 }
